d7c58519e3f1dcf0eaa930ca2d5c96aaf152ac13
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2020-07-06 11:53:18 +0100

    test(VOFController): add test endpoint

--- src/Controllers/VerintOnlineFormController.cs
+++ src/Controllers/VerintOnlineFormController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StockportGovUK.AspNetCore.Attributes.TokenAuthentication;
 using StockportGovUK.NetStandard.Models.Verint;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using verint_service.Services.VerintOnlineForm;
@@ -73,7 +74,83 @@ namespace verint_service.Controllers
         ///    }
         /// </remarks>
         [HttpPost]
-        public async Task<IActionResult> Create(VerintOnlineFormRequest model) => Ok(await _verintOnlineFormService.CreateVOFCase(model));
+        public async Task<IActionResult> Create(VerintOnlineFormRequest model) {
+            try
+            {
+                return Ok(await _verintOnlineFormService.CreateVOFCase(model));
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Creates a test case - for ease of testing on pre-production enviroments
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> TestCreate()
+        {
+            var test_model = new VerintOnlineFormRequest
+            {
+                VerintCase = new Case
+                {
+                    EventCode = 2002573,
+                    AssociatedWithBehaviour = AssociatedWithBehaviourEnum.Individual,
+                    Customer = new Customer
+                    {
+                        CustomerReference = "101003293432",
+                        Forename = "Elliott",
+                        Surname = "Humphreys"
+                    }
+                },
+                FormName = "confirm_integrationform",
+                FormData = new Dictionary<string, string>
+                {
+                    {"CONF_POC_CODE", "WEB"},
+                    {"CONF_POC_NAME","Web/Online Form"},
+                    {"CONF_METH_CODE","WEB"},
+                    {"CONF_METH_NAME","Web/Online Form"},
+                    {"CONF_CUST_REF","101003283810"},
+                    {"CONF_CUST_TITLE","Mr"},
+                    {"CONF_CUST_SURNAME","Humphreys"},
+                    {"CONF_CUST_FORENAME","Elliott"},
+                    {"CONF_CUST_PHONE","07388909179"},
+                    {"CONF_CUST_EMAIL","elliott.humphreys@stockport.gov.uk"},
+                    {"CONF_CONTACT","Mr Elliott Elliott Humphreys"},
+                    {"CONF_CONTACT_PHONE","07388909179"},
+                    {"CONF_CONTACT_EMAIL","elliott.humphreys@stockport.gov.uk"},
+                    {"CONF_CUST_BUILDING","STOCKPORT DELIVERY OFFICE 1"},
+                    {"CONF_CUST_STREET","EXCHANGE STREET"},
+                    {"CONF_CUST_LOCALITY",""},
+                    {"CONF_CUST_TOWN","STOCKPORT"},
+                    {"CONF_CUST_POSTCODE","SK1 1AA"},
+                    {"CONF_SERVICE_CODE","GREN"},
+                    {"CONF_SUBJECT_CODE","TTPO"},
+                    {"CONF_CLASSIFICATION","public_realm-greenspace-trees_request_new_tpo"},
+                    {"CboClassCode","REQU"},
+                    {"FOLLOW_UP_BY","10 Working Days"},
+                    { "CONF_DESC","(Lagan) Event Name, Request for a new Tree preservation order. \r\nEvent Name, Request for a new Tree preservation order. \r\nReason for the TPO //request,Further?/ Location Information, test 1. \r\n"},
+                    {"CONF_LOCATION","test 1"},
+                    {"CONF_SITE_CODE","38102548"},
+                    {"CONF_SITE_NAME","HIBBERT LANE"},
+                    {"CONF_SITE_LOCALITY","MARPLE"},
+                    {"CONF_SITE_TOWN","STOCKPORT"},
+                    {"EFORM_UPDATED","true"},
+                    {"VIEWMODE","C"},
+                    {"CboCustomerTypeCode","PUBL"},
+                    {"CONF_LOGGED_BY","Lagan"},
+                    {"le_eventcode","2002573"},
+                    {"le_queue_complete","AppsConfirmQueuePending"},
+                    {"le_associated_obj_type","D4"},
+                    {"le_associated_obj_id","1002109494"},
+                    { "le_description","tpo"}
+                }
+            };
+
+            return Ok(await Create(test_model));
+        }
 
 
         /* this model will be moved to StockportGovUK...Models package*/
