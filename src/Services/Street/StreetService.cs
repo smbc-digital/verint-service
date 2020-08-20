6867efdeb986591670ab2589e1e92d65f5d8c8bd
Colin Lees <colin.lees@stockport.gov.uk>
2020-08-20 16:21:51 +0100

    feat: add USRN to response from street search

--- src/Services/Street/StreetService.cs
+++ src/Services/Street/StreetService.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using StockportGovUK.NetStandard.Models.Addresses;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using StockportGovUK.NetStandard.Models.Addresses;
 using verint_service.Helpers.VerintConnection;
 using verint_service.Utils.Consts;
 using VerintWebService;
@@ -12,13 +11,10 @@ namespace verint_service.Services.Street
 {
     public class StreetService : IStreetService
     {
-        private readonly ILogger<StreetService> _logger;
-
         private readonly IVerintClient _verintConnection;
 
-        public StreetService(IVerintConnection verint, ILogger<StreetService> logger)
+        public StreetService(IVerintConnection verint)
         {
-            _logger = logger;
             _verintConnection = verint.Client();
         }
 
@@ -34,41 +30,30 @@ namespace verint_service.Services.Street
             {
                 UniqueId = reference,
                 USRN = result.USRN,
-                Name = result.BriefDetails?.ObjectDescription
+                Name = result.BriefDetails?.ObjectDescription,
+                AddressLine1 = JsonConvert.SerializeObject(result)
             };
         }
 
-        public async Task<IEnumerable<AddressSearchResult>> SearchByStreetAsync(string reference)
-        {
-            var streetSearch = new FWTStreetSearch
-            {
-                 StreetName = reference 
-            };
+        public async Task<IEnumerable<AddressSearchResult>> SearchByStreetAsync(string reference) => await DoStreetSearch(new FWTStreetSearch { StreetName = reference });
 
-            return await DoStreetSearch(streetSearch);
-        }
-
-        public async Task<IEnumerable<AddressSearchResult>> SearchByUsrnAsync(string usrn)
-        {
-            var streetSearch = new FWTStreetSearch
-            {
-                 USRN = usrn
-            };
-
-            return await DoStreetSearch(streetSearch);
-        }
+        public async Task<IEnumerable<AddressSearchResult>> SearchByUsrnAsync(string usrn) => await DoStreetSearch(new FWTStreetSearch { USRN = usrn });
 
         private async Task<IEnumerable<AddressSearchResult>> DoStreetSearch(FWTStreetSearch streetSearch)
         {
+            var streetResult = await GetUniqueId(streetSearch);
+            return streetResult.Select(_ => GetStreet(_.UniqueId).Result);
+        }
+
+        private async Task<IEnumerable<AddressSearchResult>> GetUniqueId(FWTStreetSearch streetSearch)
+        {
             var streetSearchResults = await _verintConnection.searchForStreetAsync(streetSearch);
-            var streetResults = streetSearchResults.FWTObjectBriefDetailsList.OrderBy(street => street.ObjectDescription).Select(result => new AddressSearchResult
+            return streetSearchResults.FWTObjectBriefDetailsList.OrderBy(street => street.ObjectDescription).Select(result => new AddressSearchResult
             {
                 UniqueId = result.ObjectID.ObjectReference[0],
                 Name = result.ObjectDescription,
                 AddressLine1 = JsonConvert.SerializeObject(result)
             });
-
-            return streetResults;
         }
     }
 }
